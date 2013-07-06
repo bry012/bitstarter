2d826fce602967e1a7c7ef5702f08a7e95f55f2e
bry012 <bry72991@gmail.com>
2013-07-06 07:04:19 +0000

    web.js fix

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var file_bin = fs.readFileSync("/home/ubuntu/Github/bitstarter/index.html");
+var file_bin = fs.readFileSync("/bitstarter/index.html");
 
 var bin_string = file_bin.toString();
 console.log(bin_string);
