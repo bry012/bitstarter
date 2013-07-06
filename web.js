31ab052681787be006f19343cbcdf3e34275cb17
bry012 <bry72991@gmail.com>
2013-07-06 06:34:44 +0000

    changed web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var file_bin = fs.readFileSync("/home/bryan/bitstarter/bitstarter/web.js");
+
+var bin_string = buf.toString(file_bin,0,file_bin.length);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync(bin_string));
 });
 
 var port = process.env.PORT || 5000;
