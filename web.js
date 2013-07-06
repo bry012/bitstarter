30cd1dee3e92542e7224b34812f5aafcbdba9c80
bry012 <bry72991@gmail.com>
2013-07-06 08:49:07 +0000

    test

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var file_bin = fs.readFileSync("index.html");
+var file_bin = fs.readFileSync("index2.html");
 
 var bin_string = file_bin.toString();
 console.log(bin_string);
