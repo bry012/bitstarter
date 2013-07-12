525a9a98ab7b83f828d99eff3bf1a8cdd563c983
bry012 <bry72991@gmail.com>
2013-07-12 02:27:35 +0000

    adding grader.js and checks.json

--- grader.js
+++ grader.js
@@ -22,14 +22,19 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://sleepy-garden-9793.herokuapp.com/";
+var SAVED_DEFAULT = '/home/ubuntu/Github/bitstarter/grader.txt';
 
 var assertFileExists = function(infile) {
+    
     var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
+
+    if(! fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
@@ -44,8 +49,33 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var write_file = function(chosen_file, data){
+    fs.writeFile(chosen_file, data, function(err){
+    if (err){
+            console.log(err);
+    } else {
+           console.log('File was saved to ' + chosen_file + '.');
+    }
+});
+};
+
+var checkUrl = function(url,checksfile, chosen_file){
+    rest.get(url).on('complete',function(data) {
+    $ = cheerio.load(data);
+    json_out =  check_out($,checksfile)
+    var outJ = JSON.stringify(json_out, null, 4);
+    write_file(chosen_file,outJ);
+});
+};
+
+var checkHtmlFile = function(htmlfile, checksfile, chosen_file) {
+    var $ = cheerioHtmlFile(htmlfile);
+    var checkJson =  check_out($,checksfile);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    write_file(chosen_file, outJson);
+};
+
+var check_out = function($, checksfile){
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -63,12 +93,26 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists))
+        .option('-u, --url [url]', 'Url to index.html') 
+        .option('-f, --file [html_file]', 'Path to index.html', clone(assertFileExists))
+        .option('-s --save <saved_file>', 'Path to output file', SAVED_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.file){
+	checkHtmlFile(program.file, program.checks, program.save);
+			}
+    else if (program.url){
+	checkUrl(program.url, program.checks, program.save);
+    }  
+   else {
+       console.log('No file or url entered.');
+       }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
+
+
+
